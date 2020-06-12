3c1b81f0d47864a5b8f4af1ae669a2baa700241b
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-12 01:04:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,13 +4,12 @@
 def key_for_min_value(name_hash)
 
   lowest_key = nil
-  lowest_value = nil
-  name_hash.each do |v, k|
-    if lowest_value == nil || v < lowest_value
-      lowest_value = v
-      lowest_key = k
-    end
+lowest_value = nil
+name_hash.each do |k, v|
+  if lowest_value == nil || v < lowest_value
+    lowest_value = v
+    lowest_key = k
   end
-  lowest_key
-
+end
+lowest_key
 end
